1794dd0072a3d1754889f54b5165b84351b1f847
a-elef93 <a.eleftheriadis93@gmail.com>
2026-08-17 14:07:33 +0300

    Added the code for the button numbers so they are appeared on screen when clicked

--- script.js
+++ script.js
@@ -17,3 +17,16 @@ function multiply(a , b){
 function divide(a , b){
     return a / b;
 }
+
+function displayUpdate(number){
+    // let n = parseInt(number);
+    const display = document.querySelector(".display");
+    display.textContent= number;
+}
+
+const numberButtons = document.querySelectorAll(".number");
+numberButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        displayUpdate(button.textContent);
+    });
+});
