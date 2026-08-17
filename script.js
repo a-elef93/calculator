6c1327af12c291f1ca68b8285fa20847615b714f
a-elef93 <a.eleftheriadis93@gmail.com>
2026-08-17 13:00:17 +0300

    Added the script code for the basic operations of the calculator

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 function operate(op , num1, num2 ){
-    
+
+}
+
+function add(a, b){
+    return a + b;
+}
+
+function substract(a , b){
+    return a - b;
+}
+
+function multiply(a , b){
+    return a * b;
+}
+
+function divide(a , b){
+    return a / b;
 }
