4d77fe46545543f5be366148b42a85cf32c93ab2
a-elef93 <a.eleftheriadis93@gmail.com>
2026-08-17 15:10:39 +0300

    Added a new function to update the screen when operators are clicked

--- script.js
+++ script.js
@@ -18,8 +18,8 @@ function divide(a , b){
     return a / b;
 }
 
-function displayUpdate(number){
-    // let n = parseInt(number);
+//UPDATE THE DISPLAY EVERY TIME THE USER CLICKS A NUMBER BUTTON
+function displayUpdateNumber(number){
     const display = document.querySelector(".display");
     if(display.textContent === "0")
     {
@@ -28,27 +28,43 @@ function displayUpdate(number){
     else{
         display.textContent += number;
     }
-
 }
+ //UPDATE THE DISPLAY EVERY TIME THE USER CLICK AN OPERATOR. EVERY CONSECUTIVE CLICK REPLACES THE OPERATOR SO THEY DONT STUCK 
+function displayUpdateOperator(operator){
+    console.log(operator);
+    const display = document.querySelector(".display");
+    let lastCharacter = display.textContent[display.textContent.length -1];
+    if(!isNaN(lastCharacter)){
+        display.textContent += operator;
+    }
+    else{
+        display.textContent = display.textContent.slice(0, -1) + operator;
+    }
+}
+
+//RESET THE DISPLAY TO VALUE '0' 
 function resetDisplay(){
     const display = document.querySelector(".display");
     display.textContent = "0";
 }
 
+//CLICKS ON NUMBERS
 const numberButtons = document.querySelectorAll(".number");
 numberButtons.forEach((button) => {
     button.addEventListener("click", () => {
-        displayUpdate(button.textContent);
+        displayUpdateNumber(button.textContent);
     });
 });
 
+//CLICKS ON OPERATORS
 const operatorButtons = document.querySelectorAll(".operator");
 operatorButtons.forEach((button) => {
     button.addEventListener("click", () => {
-        displayUpdate(button.textContent);
+        displayUpdateOperator(button.textContent);
     })
 })
 
+//CLICK ON CLEAR BUTTON
 const clearButton = document.querySelector(".clear");
 clearButton.addEventListener("click", () => {
     resetDisplay();
